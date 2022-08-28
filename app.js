b30d244031c11ea6f3b5e5f893667a0e2980223a
Anna Capstone <112292675+Anna-Capstone@users.noreply.github.com>
2022-08-28 15:43:52 +0800

    Update app.js

--- app.js
+++ app.js
@@ -28,12 +28,10 @@ require('./routes/fullfillmentRoutes')(app);
 if (process.env.NODE_ENV === 'production') {
    // js and css files
    // client/build will be our static folder and this will be serve by our server
-   // app.use(express.static('client/build'));
-   
    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
 
    // index.html for for all page routes
-   app.get('/', (req, res) => {
+   app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
    });
 }
