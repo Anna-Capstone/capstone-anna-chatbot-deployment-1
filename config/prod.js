14547b2cb3dfbcaaa8b32d01fa20a30f7e0e0dff
Anna Capstone <112292675+Anna-Capstone@users.noreply.github.com>
2022-08-28 15:04:58 +0800

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -3,7 +3,7 @@ module.exports = {
    dialogflow_session_id: process.env.DIALOGFLOW_SESSION_ID,
    dialogflow_session_language_code: process.env.DIALOGFLOW_SESSION_LANGUAGE_CODE,
    google_client_email: process.env.GOOGLE_CLIENT_EMAIL,
-   google_private_key: JSON.parse(process.env.GOOGLE_PRIVATE_KEY),
+//    google_private_key: JSON.parse(process.env.GOOGLE_PRIVATE_KEY),
    mongo_URI: process.env.MONGO_URI,
    jwt_secret: process.env.JWT_SECRET,
 };
